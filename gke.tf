834f1ad410299dbce8e97ab94ff1192d2503d09b
harishkalyanpur <45255320+harishkalyanpur@users.noreply.github.com>
2020-09-13 17:49:10 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -7,7 +7,6 @@ resource "google_container_cluster" "engineering" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  
   remove_default_node_pool = true
   initial_node_count       = 1
 
