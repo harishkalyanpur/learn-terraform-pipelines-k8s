254c776940680809faa49fdc70c64c51debd9a21
harishkalyanpur <45255320+harishkalyanpur@users.noreply.github.com>
2020-09-13 13:34:24 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -31,7 +31,7 @@ resource "google_container_node_pool" "engineering_preemptible_nodes" {
 
   node_config {
     preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "f1-micro"
 
     metadata = {
       disable-legacy-endpoints = "true"
